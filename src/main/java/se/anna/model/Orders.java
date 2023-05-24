7ce86173318dbf7aa4a3782c52a1d7fd795e296e
Anna Axelsson <a.axelsson51@gmail.com>
2023-05-24 20:13:12 +0200

    Add orders class

---
+++ src/main/java/se/anna/model/Orders.java
@@ -0,0 +1,52 @@
+package se.anna.model;
+
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long customer_id;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLine> orderLineList = new ArrayList<>();
+    private boolean handled;
+
+    private boolean isHandled() {
+        return this.handled;
+    }
+
+    public void setHandled(boolean handled) {
+        this.handled = handled;
+    }
+
+    public Orders(){
+        this.handled = false;
+    }
+
+    public Long getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(Long customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public void setId(Long orderId) {
+        this.id = orderId;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public List<OrderLine> getOrderLineList() {
+        return this.orderLineList;
+    }
+    
+}
+
