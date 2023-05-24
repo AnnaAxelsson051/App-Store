dec8930dcf106e114fa75b8f077ef0dad4aeabf6
Anna Axelsson <a.axelsson51@gmail.com>
2023-05-24 17:54:46 +0200

    Add functionality for authenticating admin and redirecting to admin_home

---
+++ src/main/java/se/anna/controller/AdminController.java
@@ -0,0 +1,41 @@
+package se.anna.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import se.anna.service.AdminService;
+import se.anna.service.ProductService;
+
+@Controller
+public class AdminController {
+
+    //Handels admin login
+
+    @Autowired
+    AdminService adminService;
+
+
+    @GetMapping("/adminauth")
+    public String authenticateAdmin() {
+        return "admin_auth";
+    }
+
+    @PostMapping("/adminauth")
+    public String authenticateAdmin(@RequestParam String username, @RequestParam String password, Model model){
+        if (adminService.authenticateAdminByNameAndPassword(username, password)) {
+            model.addAttribute("adminName",  adminService.getAdmin(username).getUsername());
+            return "admin_home";
+        } else {
+            return "index";
+        }
+    }
+
+    @GetMapping("/adminpage")
+    public String displayAdminHomePage(){
+        return "admin_home";
+    }
+
+}
