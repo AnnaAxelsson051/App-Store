4b2625590ae210e46a4c249ec92786d4b12bbf03
Anna Axelsson <a.axelsson51@gmail.com>
2023-05-24 18:17:55 +0200

    Add functionality for admin to add new products

---
+++ src/main/java/se/anna/controller/ProductController.java
@@ -0,0 +1,26 @@
+package se.anna.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import se.anna.model.Product;
+import se.anna.service.ProductService;
+
+@Controller
+public class ProductController {
+
+    //Handles admin adding and updating products in the store
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/newproduct")
+    public String adminAddNewProduct(Model model){
+        model.addAttribute("product", new Product());
+        return "admin_add_product";
+    }
+
+}
