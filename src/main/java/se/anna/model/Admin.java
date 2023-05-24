b4082fafbb4987aa4219ec2e96d2d70cdab68f13
Anna Axelsson <a.axelsson51@gmail.com>
2023-05-24 20:18:02 +0200

    Add model class

---
+++ src/main/java/se/anna/model/Admin.java
@@ -0,0 +1,52 @@
+package se.anna.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+@Entity
+@Table(name = "admin")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    @NotBlank
+    private String username;
+
+    @Column(name = "password")
+    @NotBlank
+    private String password;
+
+    public Admin() {
+    }
+
+    public Admin(String username, String password) {
+
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
