3d170c060d4930b8ccee01c457d054dadfea7cfa
Anna Axelsson <a.axelsson51@gmail.com>
2023-05-24 20:07:38 +0200

    Add functionality for finding handled orders in db

---
+++ src/main/java/se/anna/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package se.anna.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.anna.model.Orders;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Orders, Long> {
+
+    List<Orders> findByHandled(boolean isHandled);
+    
+}
