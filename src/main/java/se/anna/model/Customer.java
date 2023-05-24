314d3df1e5fb7699e9bc74f6c71240ffa660041e
Anna Axelsson <a.axelsson51@gmail.com>
2023-05-24 20:19:19 +0200

    Add customer class

---
+++ src/main/java/se/anna/model/Customer.java
@@ -0,0 +1,54 @@
+package se.anna.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    public Customer() {}
+
+    public Customer(String username, String password) {
+        this.username=username;
+        this.password=password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
+
+}
