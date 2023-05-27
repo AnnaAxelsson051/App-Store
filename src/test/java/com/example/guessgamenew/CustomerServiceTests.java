8ad2a0e0e217d5748d098f980358b45e844d2245
Anna Axelsson <a.axelsson51@gmail.com>
2023-05-27 14:34:41 +0200

    Add test for customer decreasing quantity in cart

--- src/test/java/com/example/guessgamenew/CustomerServiceTests.java
+++ src/test/java/com/example/guessgamenew/CustomerServiceTests.java
@@ -1,6 +1,26 @@
 package com.example.guessgamenew;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se.anna.model.CartProduct;
+import se.anna.service.CustomerService;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CustomerServiceTests {
 
+    private CustomerService customerService;
+
+    @BeforeEach
+    void setUp() {
+        customerService = new CustomerService();
+    }
+
+    @Test
+    void testDecreaseQuantityInCartFunctionality() {
+        CartProduct cartProduct = new CartProduct(1L, "Test Product 2", "210", "4");
+        customerService.getCart().add(cartProduct);
+        customerService.decreaseQuantityInCart(0);
+        assertEquals("3", customerService.getCart().get(0).getQuantity());
+    }
 
 }
